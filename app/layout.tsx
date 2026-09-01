28b1aab03450f701f1ecd914310a6ae3a0738c05
David Mikael <sylvandavidsupes@gmail.com>
2026-08-31 22:10:25 -0300

    style: conecta estilos da nova central do cliente

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./network.css";
+import "./client.css";
 
 export const metadata: Metadata = {
   title: "Parque Net Telecom",
