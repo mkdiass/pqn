9315d80417c5e03dc8f26b443a90740c4fbf0fd1
David Mikael <sylvandavidsupes@gmail.com>
2026-09-01 00:09:59 -0300

    feat: integra indicadores dinamicos na home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/navbar";
 import { Hero } from "@/components/home/hero";
 import { Benefits } from "@/components/home/benefits";
+import { Stats } from "@/components/home/stats";
 import { Plans } from "@/components/home/plans";
 import { Company } from "@/components/home/company";
 
@@ -8,13 +9,13 @@ export default function Home() {
   return (
     <>
       <Navbar />
-
       <main>
         <Hero />
         <Benefits />
+        <Stats />
         <Plans />
         <Company />
       </main>
     </>
   );
-} 
+}
