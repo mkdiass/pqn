07778a65f9634f984590e35910f4babd0358b1bc
David Mikael <sylvandavidsupes@gmail.com>
2026-08-31 21:47:22 -0300

    feat: add network experience to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/layout/navbar";
 import { Hero } from "@/components/home/hero";
+import { NetworkShowcase } from "@/components/home/network-showcase";
 import { SmartContract } from "@/components/home/smart-contract";
 import { Benefits } from "@/components/home/benefits";
 import { Plans } from "@/components/home/plans";
@@ -12,6 +13,7 @@ export default function Home() {
 
       <main>
         <Hero />
+        <NetworkShowcase />
         <SmartContract />
         <Benefits />
         <Plans />
